fa164ba45e0f69dadab116dfff12944313a89add
Theblvcksheep <semousuneo@gmail.com>
2026-01-03 01:32:32 +0200

    added streamlit app

---
+++ app/app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pickle as pkl
+import pandas as pd
+
+
+def main():
+    st.set_page_config(
+        page_title="Breast Cancer Predictor",
+        page_icon=":female-doctor:",
+        layout="wide",
+        initial_sidebar_state="expanded"
+    )
+
+    st.write("Hello world")
+
+if __name__ == '__main__':
+    main()
